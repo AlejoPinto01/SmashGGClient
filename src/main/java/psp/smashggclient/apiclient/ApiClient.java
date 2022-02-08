6c19ca36a229edcfca78b74ff5f1f9db54d57171
JaumeOS <jaumeordinas@paucasesnovescifp.cat>
2022-02-08 20:55:38 +0100

    Added Methods to get the phase id

--- src/main/java/psp/smashggclient/apiclient/ApiClient.java
+++ src/main/java/psp/smashggclient/apiclient/ApiClient.java
@@ -1,5 +1,48 @@
 package psp.smashggclient.apiclient;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ApiClient {
+    public String getTournamentPhase(String tournamentSlug) throws IOException, IOException, InterruptedException {
+        String url = "https://api.smash.gg/tournament/" + tournamentSlug + "?expand[]=phase";
+        
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String responseBody = response.body();
+        int responseCode = response.statusCode();
+
+        if (responseCode == 200) {
+            //System.out.println(responseBody);
+            return responseBody;
+        }
+        return null;
+    }
     
+    public void getEntrantsPerPhase() throws IOException, InterruptedException {
+        // 1057669 = phase_id
+        String url = "https://api.smash.gg/phase_group/" + "1057669" + "?expand[]=entrants";
+        
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String responseBody = response.body();
+        int responseCode = response.statusCode();
+
+        if (responseCode == 200) {
+            System.out.println(responseBody);
+        }
+    }
 }
--- src/main/java/psp/smashggclient/utils/Utils.java
+++ src/main/java/psp/smashggclient/utils/Utils.java
@@ -1,5 +1,19 @@
 package psp.smashggclient.utils;
 
+import java.io.IOException;
+import psp.smashggclient.apiclient.ApiClient;
+
 public class Utils {
-    
+    static ApiClient api = new ApiClient();
+
+    public void createJsonObjects() throws IOException, InterruptedException {
+        String json = api.getTournamentPhase("super-charge-shot-series");
+
+//        JsonObject obj = new JsonObject(" .... ");
+//        String pageName = obj.getJSONObject("pageInfo").getString("pageName");
+//
+//        JSONArray arr = obj.getJSONArray("posts");
+//        for (int i = 0; i < arr.length(); i++) {
+//            String post_id = arr.getJSONObject(i).getString("post_id");
+    }
 }
